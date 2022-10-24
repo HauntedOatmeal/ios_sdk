b87bdf45c2f5e6d1dfb7f523aa0858a1563f124a
HauntedOatmeal <116583817+HauntedOatmeal@users.noreply.github.com>
2022-10-24 13:51:57 -0400

    Update sw_vers.sh

--- sw_vers.sh
+++ sw_vers.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-PROD_VER='12.6'		#Replace this with the output of sw_vers -productVersion on your Mac
-BUILD_VER='21G115'	#Replace this with the output of sw_vers -buildVersion on your Mac
+PROD_VER='12.6'		#Replace this with the output of sw_vers -productVersion on your Mac, or set IOS_PROD_VER env var
+BUILD_VER='21G115'	#Replace this with the output of sw_vers -buildVersion on your Mac, or set IOS_BUILD_VER env var
 
+IPROD="${IOS_PROD_VER:-$PROD_VER}"
+IBUILD="${IOS_BUILD_VER:-$BUILD_VER}"
 case $1 in
 	"-buildVersion")
-		echo $BUILD_VER
+		echo $IBUILD
 		;;
 
 	"-productVersion")
-		echo $PROD_VER
+		echo $IPROD
 		;;
 
 	"-productName")
